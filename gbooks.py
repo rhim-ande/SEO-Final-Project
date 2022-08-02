73180a34535ad189e80829cf92d1b0af90518f43
Rhim Andemichael <rhima@princeton.edu>
2022-08-02 15:46:52 +0000

    improved version

--- gbooks.py
+++ gbooks.py
@@ -3,17 +3,16 @@ from random import sample
 
 
 def get_books():
-        url = "https://www.googleapis.com/books/v1/volumes?q={}+intitle:{}".format("black deaf culture", "Black")
-        books = requests.get(url)
-        books_data = books.json()
-        individual_books = books_data['items']
-        books_list = []
-        for book in individual_books:
-                books_list.append([book['volumeInfo']['title'], book['volumeInfo']['infoLink']])
+    url = "https://www.googleapis.com/books/v1/volumes?q={}+intitle:{}".
+    format("black deaf culture", "Black")
+    books = requests.get(url)
+    books_data = books.json()
+    individual_books = books_data['items']
+    books_list = []
+    for book in individual_books:
+        books_list.append([book['volumeInfo']['title'],
+                           book['volumeInfo']['infoLink']])
 
-        results = sample(books_list, 3)
+    results = sample(books_list, 3)
 
-        return print(results)
-
-get_books()
-
+    return results
--- gnews.py
+++ gnews.py
@@ -4,21 +4,19 @@ from random import sample
 
 apikey = os.environ.get('gn_apikey')
 
+
 def get_articles(apikey):
-    url = "https://gnews.io/api/v4/search?q={}&lang={}&token={}".format("black sign language", "en", apikey)
+    url = "https://gnews.io/api/v4/search?q={}&lang={}&token={}".
+    format("black sign language", "en", apikey)
     news = requests.get(url)
     news_data = news.json()
     each_article = news_data['articles']
     articles_list = []
     for article in each_article:
         if article['source']['name'] != 'The Nation':
-            articles_list.append([article['title'], article['description'], 
-                                article['url'], article['source']['name']])
+            articles_list.append([article['title'], article['description'],
+                                 article['url'], article['source']['name']])
 
     results = sample(articles_list, 3)
 
-    return print(results)
-
-
-get_articles(apikey)
-
+    return results
